fb676d7bc0f830d86e2879f08e182c167b329d99
JustusOmbok <ombokjustus@gmail.com>
2024-01-05 12:19:55 +0300

    ES6 promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,29 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [];
-
-  const userPromise = signUpUser(firstName, lastName)
-    .then((result) => ({
-      status: 'fulfilled',
-      value: result,
-    }))
-    .catch((error) => ({
-      status: 'rejected',
-      value: error,
-    }));
-
-  const photoPromise = uploadPhoto(fileName)
-    .then(() => ({
-      status: 'fulfilled',
-      value: `Photo ${fileName} uploaded successfully`,
-    }))
-    .catch((error) => ({
-      status: 'rejected',
-      value: error,
-    }));
-
-  promises.push(userPromise, photoPromise);
-
-  return Promise.allSettled(promises);
+  return promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((result) => (
+      result.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
