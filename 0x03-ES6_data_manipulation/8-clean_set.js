306198271978d54734ae89f86d1ddb08cf9aa54f
JustusOmbok <ombokjustus@gmail.com>
2024-01-09 09:09:40 +0300

    ES6 arrays

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,8 @@
 function cleanSet(set, startString) {
-  const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
+  const filteredValues = Array.from(set)
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length));
+
   return filteredValues.join('-');
 }
 
