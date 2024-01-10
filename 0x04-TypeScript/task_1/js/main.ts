4ff53d25c25a0b52618df94635b7491ed007e36b
JustusOmbok <ombokjustus@gmail.com>
2024-01-10 18:54:31 +0300

    Typescript

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,21 @@
+// Define the Teacher interface
+interface Teacher {
+    firstName: string;
+    lastName: string;
+    readonly fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    readonly location: string;
+    [key: string]: any;
+  }
+  
+  // Create a Teacher object
+  const teacher3: Teacher = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+  };
+  
+  // Print the Teacher object
+  console.log(teacher3);
