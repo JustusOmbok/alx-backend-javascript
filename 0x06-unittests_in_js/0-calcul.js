968a1f02ca58ff1d899cdb926d5173cc640a454b
JustusOmbok <ombokjustus@gmail.com>
2024-02-29 10:47:24 +0000

    test case

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,5 +1,7 @@
-function calculateNumber(a, b) {
+const calculateNumber = (type, a, b) => {
+  if (type === 'SUM') {
     return Math.round(a) + Math.round(b);
-}
+  }
+};
 
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,19 +4,19 @@ const calculateNumber = require('./0-calcul');
 describe('calculateNumber', () => {
   describe('type == "SUM"', () => {
     it('should return the sum of rounded numbers', () => {
-      assert.strictEqual(calculateNumber(1, 3), 4);
+      assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
     });
 
     it('should return the sum of rounded numbers (alternate)', () => {
-      assert.strictEqual(calculateNumber(1, 3.7), 5);
+      assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
     });
 
     it('should return the sum of rounded numbers (decimal)', () => {
-      assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+      assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
     });
 
     it('should return the sum of rounded numbers (decimal)', () => {
-      assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+      assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
     });
   });
 });
