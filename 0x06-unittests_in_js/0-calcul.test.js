17f9f917179128c0322ba227ba0e6b0b0105516d
JustusOmbok <ombokjustus@gmail.com>
2024-02-29 10:42:33 +0000

    testcase

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,10 +2,21 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
+  describe('type == "SUM"', () => {
     it('should return the sum of rounded numbers', () => {
-        assert.strictEqual(calculateNumber(1, 3), 4);
-        assert.strictEqual(calculateNumber(1, 3.7), 5);
-        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+      assert.strictEqual(calculateNumber(1, 3), 4);
     });
+
+    it('should return the sum of rounded numbers (alternate)', () => {
+      assert.strictEqual(calculateNumber(1, 3.7), 5);
+    });
+
+    it('should return the sum of rounded numbers (decimal)', () => {
+      assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    });
+
+    it('should return the sum of rounded numbers (decimal)', () => {
+      assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    });
+  });
 });
